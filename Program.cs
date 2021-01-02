0344bb70daca5f2bf9876a6f00ea9e68e4017428
emmaccen <oriolaemmanuel199@gmail.com>
2021-01-02 15:26:01 +0100

    Changing the world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChangingTheWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Changing the World!");
+        }
+    }
+}
+ 
